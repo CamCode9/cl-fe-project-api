7378e69eca33c1ebaf2cd8974ee4a290938824d1
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-04-20 17:10:04 +0100

    added welcome to the internet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
+        <div >
           <h1 className="centreHeader">Welcome to Real Fake News</h1>
+          <iframe className="homeVideo" width="560" height="315" src="https://www.youtube.com/embed/k1BneeJTDcU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       );
 }
